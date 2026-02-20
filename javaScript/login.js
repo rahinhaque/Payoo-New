0d2a57f32597500aa231b2ad94af8ccb1a5c9a85
Rahin Haque <haquerahin743@gmail.com>
2026-02-20 10:00:37 +0600

    GEt the pin number

--- javaScript/login.js
+++ javaScript/login.js
@@ -2,11 +2,14 @@ console.log("login done");
 
 document.getElementById("login-btn")
   .addEventListener('click' , function(e){
-    //1- Get the mobile number input
+    //1- Get the mobile number input :
     const numberInput = document.getElementById("input-number");
     const contactNumber = numberInput.value;
     console.log(contactNumber);
-    //2- get the pin number input
+    //2- get the pin number input :
+    const passwordInput = document.getElementById("input-password");
+    const pinNumber = passwordInput.value;
+    console.log(pinNumber);
     //3 -match the pin and mobile number
     //3.1 - if its true>>> Alert > Homepage
     //3.2 - if its FAlse>>> Alert > return to login page..
